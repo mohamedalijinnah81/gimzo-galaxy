ce1b0869ed77e50ae4b8f778cf144fc356e5f0e4
mohamedalijinnah81 <mohamedalijinna56@gmail.com>
2023-12-15 23:29:13 +0530

    Searchresult section optimized for mobile

--- src/components/home/modules/Listitems.js
+++ src/components/home/modules/Listitems.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Item from './Item'
 
 const Listitems = ({items}) => {
-    const firstTwelveItems = items.slice(0, 6);
+    const firstTwelveItems = items && items.slice(0, 6);
     // console.log(items);
   return (
     <ul>
-        {firstTwelveItems.map((item) => (
+        {firstTwelveItems && firstTwelveItems.map((item) => (
             <Item 
                 item={item}
                 key={item.asin}
